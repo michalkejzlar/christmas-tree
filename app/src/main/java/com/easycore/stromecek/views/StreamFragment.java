476d25d7781369a6d9651911a609f34a4f799792
Michal Kejzlar <kejzlarmichal@gmail.com>
2016-12-12 22:19:53 +0100

    Simplify layout in bottom sheet

--- app/src/main/java/com/easycore/stromecek/views/StreamFragment.java
+++ app/src/main/java/com/easycore/stromecek/views/StreamFragment.java
@@ -18,7 +18,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 import butterknife.BindView;
@@ -78,7 +77,7 @@ public final class StreamFragment extends Fragment {
     @BindView(R.id.companyDesc)
     protected TextView companyDescTxtView;
     @BindView(R.id.bottomSheetButton)
-    protected Button bottomSheetButton;
+    protected TextView bottomSheetButton;
 
     private BottomSheetBehavior bottomSheetBehavior;
 
